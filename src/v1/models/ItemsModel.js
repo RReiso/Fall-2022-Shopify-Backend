8b92cc85ee624546662a5cb0461f840411b4858c
Ruta Reisoglu <rutareiso@protonmail.com>
2022-05-16 15:35:48 -0400

    Modify schema

--- src/v1/models/ItemsModel.js
+++ src/v1/models/ItemsModel.js
@@ -12,7 +12,7 @@ const itemsModel = mongoose.Schema(
     isDeleted: { type: Boolean, default: false },
     deletionComments: { type: String, trim: true },
     money: {
-      price: { type: Number, min: 0.01 },
+      price: { type: Number, min: 0 },
       currency: { type: String, enum: ["USD", "CAD", "", "kk"] },
     },
     inStock: { type: Number, required: true, min: 0 },
